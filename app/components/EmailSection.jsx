938551ea9d2d700aabdf82be515b2a82775759d2
Andrew Lo <lo.andrew.ch@gmail.com>
2025-11-03 17:43:00 -0500

    save

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -87,7 +87,7 @@ const EmailSection = () => {
                 id="email"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="jacob@google.com"
+                placeholder="example@email.com"
               />
             </div>
             <div className="mb-6">
@@ -128,6 +128,7 @@ const EmailSection = () => {
             </button>
           </form>
         )}
+        <div className="font-bold text-white my-2">lo.andrew.ch@gmail.com</div>
       </div>
     </section>
   );
