04e4e5ea68d0e743697c9f8c6cc43652348a05a6
Andrew Lo <lo.andrew.ch@gmail.com>
2025-11-06 16:37:49 -0500

    new picture

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -38,7 +38,7 @@ export default function HeroSection() {
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
             <Image
-              src="/images/headshot.png"
+              src="/images/headshot2.png"
               alt="picture of my head"
               className="rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={250}
